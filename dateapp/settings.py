f3692b655e600b3edf83b8b151730b3cf17c89b7
minwoo <alsdndia789@ajou.ac.kr>
2022-03-06 11:20:58 +0900

    Decouple

--- dateapp/settings.py
+++ dateapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = config("SECRET_KEY")
 SECRET_KEY = 'django-insecure-2r4i&1(1=8%oty694+a&$u5dyxejo40pza1+u$dc452kyy^dsd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -78,12 +79,10 @@ WSGI_APPLICATION = 'dateapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": config("ENGINE"),
+        "NAME": BASE_DIR/config("NAME"),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
